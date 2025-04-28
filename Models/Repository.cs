1d89d402c257026be5252d39dbf585130d57fdfe
Furkan <furkanoznamli10@email.com>
2025-04-28 11:40:40 +0300

    Add project files.

---
+++ Models/Repository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MeetingApp.Models
+{
+    public static class Repository 
+    {
+       private static List<UserInfo> _users = new();
+
+        static Repository()
+        {
+            _users.Add(new UserInfo() { Id = 1, Name = "Furkan", Email = "asd@gmail.com", Phone = "4440444", WillAttend = true });
+            _users.Add(new UserInfo() { Id = 2, Name = "Fatih", Email = "vsa@gmail.com", Phone = "4440333", WillAttend = false });
+            _users.Add(new UserInfo() { Id = 3, Name = "Hasan", Email = "fgh@gmail.com", Phone = "4440222", WillAttend = true });
+
+        }
+        public static List<UserInfo> Users { 
+            get { return _users; }
+        }
+
+        public static void CreateUser(UserInfo user)
+        {
+            user.Id = Users.Count + 1;
+            _users.Add(user);   
+        }
+
+        public static UserInfo? GetById(int id)
+        {
+            return _users.FirstOrDefault(user => user.Id == id);
+        }
+    }
+}
---
+++ Models/UserInfo.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace MeetingApp.Models
+{
+    public class UserInfo
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Ad Soyad alanı boş bırakılamaz")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage ="Telefon alanı boş bırakılamaz")]
+        public string? Phone { get; set; }
+
+        [Required(ErrorMessage ="Email alanı boş bırakılamaz")]
+        [EmailAddress(ErrorMessage ="Hatalı Email Formatı")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage ="Lütfen katılım durumu seçiniz")]
+        public bool? WillAttend {  get; set; }
+        
+    }
+}
